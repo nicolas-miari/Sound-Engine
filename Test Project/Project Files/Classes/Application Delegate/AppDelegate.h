eb4c62630f80e57931c348a277d15fcda4378c55
Nicolás Miari <nicolasmiari@gmail.com>
2014-03-13 13:36:16 +0900

    Wrapped the 2 source files in a test Xcode project.

---
+++ Test Project/Project Files/Classes/Application Delegate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SoundEngineTest
+//
+//  Created by n.miari on 3/13/14.
+//  Copyright (c) 2014 Nicolas Miari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test Project/Project Files/Other Sources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SoundEngineTest
+//
+//  Created by n.miari on 3/13/14.
+//  Copyright (c) 2014 Nicolas Miari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
